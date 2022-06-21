eba82236d18169a454f70d6e3f7c426d483315bb
AdamHafizhAbdillah <80688249+AdamHafizhAbdillah@users.noreply.github.com>
2022-06-21 11:12:04 +0700

    update main

--- mobile-majestic-banyuangi-main/lib/main.dart
+++ mobile-majestic-banyuangi-main/lib/main.dart
@@ -1,11 +1,21 @@
 import 'package:flutter/material.dart';
-import 'package:majestic_banyuangi/pages/detail_page.dart';
-import 'package:majestic_banyuangi/pages/home_page.dart';
-import 'package:majestic_banyuangi/pages/profile_page.dart';
-import 'package:majestic_banyuangi/pages/signin_page.dart';
-import 'package:majestic_banyuangi/pages/signup_page.dart';
-import 'package:majestic_banyuangi/pages/splash_page.dart';
-import 'package:majestic_banyuangi/pages/update_page.dart';
+import 'package:majestic/pages/detail_page.dart';
+import 'package:majestic/pages/home_page.dart';
+import 'package:majestic/pages/order_page.dart';
+import 'package:majestic/pages/payment_page.dart';
+import 'package:majestic/pages/profile_page.dart';
+import 'package:majestic/pages/signin_page.dart';
+import 'package:majestic/pages/signup_page.dart';
+import 'package:majestic/pages/splash_page.dart';
+import 'package:majestic/pages/update_page.dart';
+import 'package:majestic/providers/auth_provider.dart';
+import 'package:majestic/providers/checkout_provider.dart';
+import 'package:majestic/providers/destination_provider.dart';
+import 'package:majestic/providers/detail_provider.dart';
+import 'package:majestic/providers/event_provider.dart';
+import 'package:majestic/providers/order_provider.dart';
+import 'package:majestic/providers/payment_provider.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(const MyApp());
@@ -17,19 +27,46 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Majestic Banyuangi',
-      debugShowCheckedModeBanner: false,
-      initialRoute: '/',
-      routes: {
-        '/': (context) => const SplashPage(),
-        '/signin': (context) => const SigninPage(),
-        '/signup': (context) => const SignupPage(),
-        '/home': (context) => const HomePage(),
-        '/detail': (context) => const DetailPage(),
-        '/profile': (context) => const ProfilePage(),
-        '/update-profile': (context) => const UpdatePage(),
-      },
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (context) => AuthProvider(),
+        ),
+        ChangeNotifierProvider(
+          create: (context) => DestinationProvider(),
+        ),
+        ChangeNotifierProvider(
+          create: (context) => PaymentProvider(),
+        ),
+        ChangeNotifierProvider(
+          create: (context) => DetailProvider(),
+        ),
+        ChangeNotifierProvider(
+          create: (context) => EventProvider(),
+        ),
+        ChangeNotifierProvider(
+          create: (context) => OrderProvider(),
+        ),
+        ChangeNotifierProvider(
+          create: (context) => CheckoutProvider(),
+        ),
+      ],
+      child: MaterialApp(
+        title: 'Majestic majestic',
+        debugShowCheckedModeBanner: false,
+        initialRoute: '/',
+        routes: {
+          '/': (context) => const SplashPage(),
+          '/signin': (context) => const SigninPage(),
+          '/signup': (context) => const SignupPage(),
+          '/home': (context) => const HomePage(),
+          '/profile': (context) => const ProfilePage(),
+          '/detail': (context) => const DetailPage(),
+          '/update-profile': (context) => const UpdatePage(),
+          '/payment': (context) => const PaymentPage(),
+          '/order': (context) => const OrderPage(),
+        },
+      ),
     );
   }
 }
